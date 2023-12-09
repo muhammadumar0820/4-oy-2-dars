0b2ce199e6789201c7856bc2cff3a2c5619df67b
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-09 20:08:32 +0300

    masalalar bajarildi

---
+++ index.js
@@ -0,0 +1,81 @@
+//Birinchi masala.
+//let num1 = +prompt("Birinchi sonni kiriting...");
+//let num2 = +prompt("Ikkinchi sonni kiriting...");
+//let num3 = +prompt("Uchinchi sonni kiriting...");
+
+//let max1;
+//if (num1 > num2) {
+//    max1 = num1;
+//} else{
+//    max1 = num2;
+//}
+
+//let maximal;
+//if (max1 > num3) {
+//    maximal=max1;
+//} else{
+//    maximal=num3;
+//}
+
+//console.log(maximal);
+
+
+
+//Ikkinchi masala.
+//let num1 = +prompt("Birinchi sonni kiriting");
+//let num2 = +prompt("Ikkinchi sonni kiriting");
+
+//let max;
+//if (num1>num2) {
+//    max=num1;
+//}else{
+//    max=num2;
+//}
+//console.log(max ** 3);
+
+
+
+//Uchinchi masala
+//let num1 = +prompt(Birinchi sonni kiriting);
+//let num2 = +prompt(Ikkinchi sonni kiriting);
+//let num3 = +prompt(Uchinchi sonni kiriting);
+
+//let odd;
+//if (num1 % 2 == 1) {
+//    console.log(num1);
+//}
+
+//if (num2 % 2 == 1) {
+//    console.log(num2);
+//}
+
+//if (num3 % 2 == 1) {
+//    console.log(num3);
+//}
+
+//if (num1,num2 % 2 == 1) {
+//    console.log(num1 + num2);
+//}
+
+//if (num1,num3 % 2 == 1) {
+//    console.log(num1 + num3);
+//}
+
+//if (num2,num3 % 2 == 1) {
+//    console.log(num2 + num3);
+//}
+
+//if (num1,num2,num3 % 2 == 1) {
+//    console.log(num1 + num2 + num3);
+//}
+
+
+
+//Beshinchi masala
+//let num = +prompt("sonni kiriting...")
+
+//if (num % 3 , 5 == 0) {
+//    console.log(true);
+//}else {
+//    console.log(false);
+//}
